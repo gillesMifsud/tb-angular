5bbdcb953f000018d7ad8f82cc3adac209f44d51
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2017-07-06 15:27:20 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  titleChanged: boolean = false;
+  randomColor: string = '';
+
+  public changeTitle() {
+    this.title = 'new title!';
+    this.titleChanged = true;
+  }
+
+  public getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++){
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  //function to set a new random color
+  public setColor() {
+    this.randomColor = this.getRandomColor();
+  }
 }
